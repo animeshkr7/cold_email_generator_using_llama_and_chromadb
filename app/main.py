8b58ac8f4f3319dd331fe3bfbbd98098ccf120d2
Animesh Kumar Singh <animeshkr7@gmail.com>
2024-09-03 00:44:49 +0530

    updated app.chainsand port for main for render

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
-from chains import Chain
-from portfolio import Portfolio
+from app.chains import Chain
+from app.portfolio import Portfolio
 from utils import clean_text
 
 
