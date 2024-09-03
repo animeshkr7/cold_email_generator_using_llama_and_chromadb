f5508f5a62cd3432702622bf362409c0231c967e
Animesh Kumar Singh <animeshkr7@gmail.com>
2024-09-03 15:22:38 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
-from app.chains import Chain
-from app.portfolio import Portfolio
-from app.utils import clean_text
+from module.chains import Chain
+from module.portfolio import Portfolio
+from module.utils import clean_text
 
 
 def create_streamlit_app(llm, portfolio, clean_text):
